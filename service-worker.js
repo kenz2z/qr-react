7cb4165c658293f1be450b135eaa3658bf0f688f
kenz2z <khui@gimmickhouse.com>
2022-06-22 17:24:59 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/qr-react/precache-manifest.491f69d6aad9e273466497fea6b1ebbb.js"
+  "/qr-react/precache-manifest.4876be46c8a2fa0e4d4520714718b6d5.js"
 );
 
 workbox.clientsClaim();
